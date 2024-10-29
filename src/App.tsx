621237c0f6cdb02372d21d4d23147bb15a7a7dd7
gaoooon <gaon9749@gmail.com>
2024-10-29 19:54:43 +0900

    feat : 프로젝트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-const  App = () => {
-  return <div></div>;
-}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 600px;
+  height: 100%;
+  margin: 0 auto;
+
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+`;
+
+const App = () => {
+  return (
+    <Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<></>} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { GlobalStyle } from '@/styles';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <>
+    <App />
+    <GlobalStyle />
+  </>
 );
