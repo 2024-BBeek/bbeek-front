8db339086b6620c57ed6fcb28af5a4b07adbece0
gaoooon <gaon9749@gmail.com>
2024-10-29 19:25:56 +0900

    feat : 프로젝트 세팅

---
+++ sw.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+self.addEventListener("install", (e) => {
+  // console.log('[Service Worker] installed');
+});
+
+self.addEventListener("activate", (e) => {
+  // console.log('[Service Worker] actived', e);
+});
+
+self.addEventListener("fetch", (e) => {
+  // console.log('[Service Worker] fetched resource ' + e.request.url);
+});
