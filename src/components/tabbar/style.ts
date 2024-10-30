bcba415cbaff95322b833a251125e53a1dad9123
은지 <wej0513@dsm.hs.kr>
2024-10-30 11:37:34 +0900

    feat : 탭바 퍼블리싱

---
+++ src/components/tabbar/style.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 3.75rem;
+  border-top: solid 1px #bdbdbd;
+  padding: 0.75rem 10.5rem;
+`;
+
+export const IconWrapper = styled.div``;
+
+export const Icon = styled.img`
+  width: 1.5625rem;
+  height: 1.5625rem;
+`;
+
+export const Title = styled.p`
+  font-size: 0.625rem;
+  color: #9e9e9e;
+`;
